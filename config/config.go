579a682a93449e7f6a1f43fef6283895c5d7d0a7
runarmyklebust <rmy@enonic.com>
2018-06-27 12:37:08 +0200

    Removed main

--- config/config.go
+++ config/config.go
@@ -1,31 +1,7 @@
 package config
 
-import (
-	"github.com/go-yaml/yaml"
-	"io/ioutil"
-)
-
 type ClientConfig struct {
 	EndpointURL string `yaml:"endpointUrl"`
 	AuthToken   string `yaml:"authToken"`
 }
 
-func Create(fileName string) (*ClientConfig, error) {
-	return getFileConfig(fileName)
-}
-
-func getFileConfig(fileName string) (*ClientConfig, error) {
-	data, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		return nil, err
-	}
-
-	config := new(ClientConfig)
-	marshalErr := yaml.Unmarshal(data, config)
-
-	if marshalErr != nil {
-		return nil, marshalErr
-	}
-
-	return config, nil
-}
